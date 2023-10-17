883178af24a75c560d58ee692f9ca66046eb36e4
JISMI5SAJU <146074614+JISMI5SAJU@users.noreply.github.com>
2023-10-17 14:18:13 +0530

    Add files via upload

---
+++ overloading.java
@@ -0,0 +1,39 @@
+package javaprograms;
+import java.util.Scanner;
+public class overloading {
+	public static void main(String[]args) {
+		int length,breadth;
+		float height,base,radius;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter length and breadth of the rectangle");
+		length=sc.nextInt();
+		breadth=sc.nextInt();
+		System.out.println("Enter heightand base of triangle");
+		height=sc.nextFloat();
+		base=sc.nextFloat();
+		System.out.println("Enter radius of circle");
+		radius=sc.nextFloat();
+		shape area1=new shape();
+		area1.area(length,breadth);
+		area1.area(base,height);
+		area1.area(radius);
+		
+	}
+		
+	}
+	class shape{
+		public void area(int length,int breadth) {
+			System.out.println("Area of rectangle="+(length*breadth));
+		}
+		public void area(float base,float height) {
+			System.out.println("Area of triangle="+(0.5*base*height));
+		}
+		public void area(float radius) {
+			System.out.println("Area of circle="+3.14*radius*radius);
+		}
+
+				
+		
+	}
+
+
